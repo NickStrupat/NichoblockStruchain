083bd71cdbcca0511027ea2a8b897590b8e5c759
NickStrupat <nick.strupat@gmail.com>
2018-12-08 16:20:52 -0500

    random clean-up

--- src/NodeWebApi/Startup.cs
+++ src/NodeWebApi/Startup.cs
@@ -13,26 +13,18 @@ namespace NodeWebApi
 		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-		}
+		public void ConfigureServices(IServiceCollection services) =>
+			services
+				.AddMvc()
+				.SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+		IApplicationBuilder UseDevOrProd(IApplicationBuilder app, IHostingEnvironment env) => env.IsDevelopment() ? app.UseDeveloperExceptionPage() : app.UseHsts();
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-
-			app.UseHttpsRedirection();
-			app.UseMvc();
-		}
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env) =>
+			UseDevOrProd(app, env)
+				.UseHttpsRedirection()
+				.UseMvc();
 	}
 }
