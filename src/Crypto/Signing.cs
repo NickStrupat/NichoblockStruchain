8d48fae6248edcf113373a49014ce802a325f0f3
NickStrupat <nick.strupat@gmail.com>
2019-10-06 21:07:19 -0400

    add NSec crypto library for span-based API and matching signature support with tweet-nacl.js

---
+++ src/Crypto/Signing.cs
@@ -0,0 +1,23 @@
+﻿using NSec.Cryptography;
+using System;
+
+namespace Crypto
+{
+	public static class Signing
+	{
+		private static readonly Ed25519 algorithm =
+			SignatureAlgorithm.Ed25519;
+
+		public static Int32 SignatureSize =>
+			algorithm.SignatureSize;
+
+		public static Key CreateKeyPair() =>
+			Key.Create(algorithm);
+
+		public static void Sign(in Key key, in ReadOnlySpan<Byte> data, in Span<Byte> signature) =>
+			algorithm.Sign(key, data, signature);
+
+		public static Boolean Verify(in PublicKey publicKey, in ReadOnlySpan<Byte> data, in ReadOnlySpan<Byte> signature) =>
+			algorithm.Verify(publicKey, data, signature);
+	}
+}
